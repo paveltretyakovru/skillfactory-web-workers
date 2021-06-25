3fd1211a531b3c387c89971fc4302b1fe6a0743a
Pavel Tretyakov <az@paveltretyakov.ru>
2021-06-25 03:04:35 +0300

    Добавлена прослуша ошибки воркера

--- worker-example/worker.js
+++ worker-example/worker.js
@@ -1,6 +1,9 @@
 addEventListener('message', (event) => {
   console.log('Message from main streaml:', event.data);
   console.log('Начало сложных вычислений');
+
+  throw new Error('AAAAA PANIC AT THE DISCO!');
+
   for (let i = 0, max = 10000000; i < max; i++) {
     const date = new Date();
   }
